62564b073c20fca2fd489c58a4b1dfd4cb11a4b9
Josue Figueroa <poor.rican@pm.me>
2023-07-18 00:55:36 -0700

    Added documentation/examples to `ESearch`

--- src/eutils/esearch.rs
+++ src/eutils/esearch.rs
@@ -6,6 +6,46 @@ use crate::eutils::eutil::EUtil;
 use crate::parsing::XmlNode;
 
 #[derive(Debug, PartialEq)]
+/// Enables searching of NCBI databases
+///
+/// What is returned by this API is a list of ID's that
+/// are used to get the biological data associated with the query.
+///
+/// # Examples
+///
+/// ### A basic search query:
+///
+/// ```
+/// use ncbi::{EntrezDb, ESearch, EUtil};
+/// let results = ESearch::new(EntrezDb::Gene)
+///                     .term("decarboxylase")
+///                     .search();
+/// assert_eq!(results.unwrap().ret_max(), 20);
+/// ```
+///
+/// ### A bulk search:
+///
+/// ```
+/// use ncbi::{EntrezDb, ESearch, EUtil};
+/// let results = ESearch::new(EntrezDb::Protein)
+///                     .term("decarboxylase")
+///                     .max(10000)
+///                     .search();
+/// assert_eq!(results.unwrap().ret_max(), 10000);
+/// ```
+///
+/// ### A paginated search:
+///
+/// ```
+/// use ncbi::{EntrezDb, ESearch, EUtil};
+/// let results = ESearch::new(EntrezDb::Protein)
+///                     .term("decarboxylase")
+///                     .start(1000)
+///                     .max(500)
+///                     .search();
+/// assert_eq!(results.as_ref().unwrap().ret_start(), 1000);
+/// assert_eq!(results.as_ref().unwrap().ret_max(), 500);
+/// ```
 pub struct ESearch<'a> {
     db: EntrezDb,
     term: Option<&'a str>,
@@ -17,26 +57,109 @@ pub struct ESearch<'a> {
 impl<'a> ESearch<'a> {
     const ENDPOINT: &'static str = "esearch.fcgi?";
 
+    /// Builder method for search "term"
+    ///
+    /// Those familiar with the NCBI query syntax are able to
+    /// pass a full query string as the term.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use ncbi::{ESearch, EntrezDb, EUtil};
+    ///
+    /// let term = "enzyme modification transport fungi";
+    /// let builder = ESearch::new(EntrezDb::Genome)
+    ///                 .term(term);
+    /// let url = builder.build_url();
+    /// assert!(url.as_str()
+    ///            .contains(term.replace(' ', "+")
+    ///            .as_str()))
+    /// ```
     pub fn term(mut self, term: &'a str) -> Self {
         self.term = Some(term);
         self
     }
 
+    /// Builder method for "retstart"
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use ncbi::{ESearch, EntrezDb, EUtil};
+    ///
+    /// let builder = ESearch::new(EntrezDb::Genome)
+    ///                 .term("")
+    ///                 .start(400);
+    /// let url = builder.build_url();
+    /// assert!(url.as_str()
+    ///            .contains("?retstart=400"))
+    /// ```
     pub fn start(mut self, ret_start: usize) -> Self {
         self.ret_start = Some(ret_start);
         self
     }
 
+    /// Builder method for "retmax"
+    ///
+    /// # Example
+    /// ```
+    /// use ncbi::{ESearch, EntrezDb, EUtil};
+    ///
+    /// let builder = ESearch::new(EntrezDb::Genome)
+    ///                 .term("")
+    ///                 .max(400);
+    /// let url = builder.build_url();
+    /// assert!(url.as_str()
+    ///            .contains("?retmax=400"))
+    /// ```
     pub fn max(mut self, ret_max: usize) -> Self {
         self.ret_max = Some(ret_max);
         self
     }
 
+    /// Builder method for "field"
+    ///
+    /// This string restricts the search to an entire field/column of
+    /// the database. The available values for these fields vary
+    /// across databases.
+    ///
+    /// ## Note
+    ///
+    /// This value is poorly documented, but those familiar with
+    /// the NCBI query syntax would be able to use this term.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use ncbi::{ESearch, EntrezDb, EUtil};
+    ///
+    /// // this is a poor example of this field
+    /// let builder = ESearch::new(EntrezDb::Genome)
+    ///                 .term("bacteria")
+    ///                 .field("organism");
+    /// let url = builder.build_url();
+    /// assert!(url.as_str()
+    ///            .contains("?retmax=400"))
+    /// ```
     pub fn field(mut self, field: &'a str) -> Self {
         self.field = Some(field);
         self
     }
 
+    #[must_use]
+    /// Executes search query and returns [`ESearchResult`]
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use ncbi::{ESearch, EntrezDb, EUtil, ESearchResult};
+    ///
+    /// // note
+    /// let results = ESearch::new(EntrezDb::Genome)
+    ///                 .term("bacteria")
+    ///                 .search();
+    /// assert!(results.unwrap().count() >= 1);
+    /// ```
     pub fn search(&self) -> Option<ESearchResult> {
         let response = self.get();
         let mut reader = Reader::from_str(response.as_str());
