10e1a5ee41dff66629c60453db4f758dc0c4894e
Josue Figueroa <poor.rican@pm.me>
2023-07-20 00:46:20 -0700

    Added documentation and examples to `EFetch`

--- src/eutils/efetch.rs
+++ src/eutils/efetch.rs
@@ -1,12 +1,16 @@
+//! Retrieve and handle data from NCBI
+use quick_xml::events::Event;
+use quick_xml::Reader;
 use reqwest::Url;
-use crate::{EntrezDb, EUtil};
+use crate::{EntrezDb, ESearchResult, EUtil};
 use crate::eutils::{BASE, SEQUENCE_DB};
+use crate::parsing::XmlNode;
 use crate::seqset::BioSeqSet;
 
 type RetType = &'static str;
 type RetMode = &'static str;
 
-
+#[non_exhaustive]
 /// Encapsulate high-level types returned by EUtils
 ///
 /// This captures the possible return values from parsed XML. While each NCBI
@@ -18,8 +22,6 @@ pub enum DataType {
     EtAl,
 }
 
-/// # See Also
-///
 /// [Valid values of &retmode and &rettype for EFetch](https://www.ncbi.nlm.nih.gov/books/NBK25499/table/chapter4.T._valid_values_of__retmode_and/?report=objectonly)
 fn check_return_type_mode(db: EntrezDb) -> (RetType, RetMode) {
     match db {
@@ -32,6 +34,30 @@ fn check_return_type_mode(db: EntrezDb) -> (RetType, RetMode) {
     }
 }
 
+/// Retrieves biological sequence data and it's metadata from NCBI
+///
+/// # Example
+///
+/// ```
+/// // accession version numbers for E. coli RL465 plasmid's I, II, III
+/// use std::ptr::replace;
+/// use ncbi::{DataType, EFetch, EntrezDb, EUtil};
+/// let acc = [
+///     "NZ_LT906558.1",
+///     "NZ_LT906557.1",
+///     "NZ_LT906556.1",
+/// ];
+/// let response = EFetch::new(EntrezDb::Nucleotide)
+///                     .ids(&acc)
+///                     .fetch();
+///
+/// if let DataType::BioSeqSet(set) = response.unwrap() {
+///     println!("{:?}", set)
+/// } else {
+///     assert!(false)
+/// }
+/// ```
+///
 /// # See Also
 ///
 /// [Chapter on EFetch syntax](https://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.EFetch)
@@ -53,11 +79,38 @@ impl EFetch {
     const ENDPOINT: &'static str = "efetch.fcgi?";
 
     #[must_use]
+    /// Builder method for `id` field
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use ncbi::{EFetch, EntrezDb, EUtil};
+    ///
+    /// let builder = EFetch::new(EntrezDb::Genome)
+    ///                 .id("1".to_string());
+    /// let url = builder.build_url();
+    /// assert!(url.as_str()
+    ///            .contains("&id=1"))
+    /// ```
     pub fn id(mut self, id: String) -> Self {
         self.id = Some(id);
         self
     }
 
+    /// Builder method for passing a slice of &str values
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use ncbi::{EFetch, EntrezDb, EUtil};
+    ///
+    /// let builder = EFetch::new(EntrezDb::Genome)
+    ///                 .ids(&["1", "2"]);
+    /// let url = builder.build_url();
+    /// // url contains url-encoded, but comma-separated string
+    /// assert!(url.as_str()
+    ///            .contains("&id=1%2C2"))
+    /// ```
     pub fn ids(self, ids: &[& str]) -> Self {
         let mut concatenated = String::new();
 
@@ -73,14 +126,94 @@ impl EFetch {
         self.id(concatenated)
     }
 
+    /// Builder method for setting id term from an [`ESearchResult`]
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use ncbi::{EFetch, EntrezDb, ESearchResult, EUtil};
+    ///
+    /// let db = EntrezDb::Genome;
+    /// let result = ESearchResult::new( 2, 2, 0, vec![
+    ///     "1".to_string(),
+    ///     "2".to_string() ]);
+    ///
+    /// let builder = EFetch::new(db)
+    ///                 .ids_from_result(result);
+    /// let url = builder.build_url();
+    /// // url contains url encoded, but comma-separated string
+    /// assert!(url.as_str()
+    ///            .contains("&id=1%2C2"))
+    /// ```
+    pub fn ids_from_result(self, result: ESearchResult) -> Self {
+        let mut concatenated = String::new();
+        let mut iter = result.iter_id_list();
+
+        if let Some(first) = iter.next() {
+            concatenated.push_str(first.as_str());
+        }
+        iter.for_each(|i| {
+            concatenated.push_str(",");
+            concatenated.push_str(i.as_str());
+        });
+
+        self.id(concatenated)
+    }
+
+    /// Builder method for "retstart"
+    ///
+    /// # Example
+    /// ```
+    /// use ncbi::{EFetch, EntrezDb, EUtil};
+    ///
+    /// let builder = EFetch::new(EntrezDb::Genome)
+    ///                 .id("".to_string())
+    ///                 .start(777);
+    /// let url = builder.build_url();
+    /// assert!(url.as_str()
+    ///            .contains("&retstart=777"))
+    /// ```
     pub fn start(mut self, ret_start: usize) -> Self {
         self.ret_start = Some(ret_start);
         self
     }
 
+
+    /// Builder method for "retmax"
+    ///
+    /// # Example
+    /// ```
+    /// use ncbi::{EFetch, EntrezDb, EUtil};
+    ///
+    /// let builder = EFetch::new(EntrezDb::Genome)
+    ///                 .id("".to_string())
+    ///                 .max(777);
+    /// let url = builder.build_url();
+    /// assert!(url.as_str()
+    ///            .contains("&retmax=777"))
+    /// ```
     pub fn max(mut self, ret_max: usize) -> Self {
         self.ret_max = Some(ret_max);
         self
+    }
+
+    pub fn fetch(&self) -> Option<DataType> {
+        let response = self.get();
+        let mut reader = Reader::from_str(response.as_str());
+
+        loop {
+            match reader.read_event().unwrap() {
+                Event::Start(e) => {
+                    if e.name() == BioSeqSet::start_bytes().name() {
+                        let set = BioSeqSet::from_reader(&mut reader).unwrap();
+                        return Some(DataType::BioSeqSet(set));
+                    }
+                }
+                Event::Eof => break,
+                _ => (),
+            }
+        }
+        return None;
     }
 }
 
